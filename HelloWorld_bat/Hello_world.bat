277d330e8b468e324f0a60749d573be6854e1e5d
Chel <cdeszz8@gmail.com>
2024-02-26 17:22:50 -0600

    cls added

--- HelloWorld_bat/Hello_world.bat
+++ HelloWorld_bat/Hello_world.bat
@@ -1,21 +1,27 @@
 @echo off
+cls
 echo 1_Hello World
 pause
 
+cls
 echo 2_Lista las cosas en el directorio
 dir
 pause
 
+cls
 echo 3_Creando el directorio Test
 mkdir Test
 pause
 
+cls
 echo 4_Nos cambiamos al subdirectorio creado
 cd Test
 pause
 
+cls
 echo 5_Listando las cosas en el subdirectorio
 dir
 pause
 
+cls
 echo Batch script terminado
