aa48fdcee8c19bb77a75f92152bf360d69666637
Chel <cdeszz8@gmail.com>
2024-02-26 17:13:27 -0600

    2.0 of bat archive, workin properly

---
+++ HelloWorld_bat/Hello_world.bat
@@ -0,0 +1,21 @@
+@echo off
+echo 1_Hello World
+pause
+
+echo 2_Lista las cosas en el directorio
+dir
+pause
+
+echo 3_Creando el directorio Test
+mkdir Test
+pause
+
+echo 4_Nos cambiamos al subdirectorio creado
+cd Test
+pause
+
+echo 5_Listando las cosas en el subdirectorio
+dir
+pause
+
+echo Batch script terminado
